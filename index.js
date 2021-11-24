6bd84b944ace0c080407da048f5d3e8ad6ef2833
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-11-24 10:34:33 -0300

    added function "suma"

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function showNumber(number) {
     console.log("el número ingresado es " + " " + number);
 };
+
+function suma(number1,number2) {
+   console.log(number1 + number2);
+   return number1 + number2;
+};
+
+suma(4,5);
